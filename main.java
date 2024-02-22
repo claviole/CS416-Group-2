eb2f7ea7d7d7c009f3dd2098e11868656c21ab34
glimbach <160186675+glimbach@users.noreply.github.com>
2024-02-22 16:02:30 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+public static  char convertToAscii (int a){
+char ascii = (char)a;
+return ascii;
 }
 
