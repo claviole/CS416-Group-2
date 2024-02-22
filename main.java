908c5070bb62980a1cb0bee6410c1444fcb27262
AlecMalenfant001 <malenfantalec@gmail.com>
2024-02-22 15:51:39 -0600

    Added the function alecFunction()

--- main.java
+++ main.java
@@ -2,6 +2,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        alecsFunction();
+    }
+
+    private static void alecsFunction() {
+        System.out.println("This is for lab 3 in CS 41600 \n:)");
     }
 }
-
