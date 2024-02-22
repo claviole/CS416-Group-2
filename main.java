41ce457b65b0db59c01ec3dfe6b4d1eb15db5beb
AlecMalenfant001 <malenfantalec@gmail.com>
2024-02-22 16:38:27 -0600

    removed smiley

--- main.java
+++ main.java
@@ -1,17 +1,16 @@
 
-
 public class main {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
     }
-    
-    public static  char convertToAscii (int a){
-    char ascii = (char)a;
-    return ascii;
+
+    public static char convertToAscii(int a) {
+        char ascii = (char) a;
+        return ascii;
     }
-    
-    //Christian Laviolette
+
+    // Christian Laviolette
     public static void christian(String[] args) {
         System.out.println("Hello, World!");
 
@@ -21,21 +20,20 @@ public class main {
     }
 
     public static int add(int x, int y) {
-        return x+y;
-    }
-  
-  private static void alecsFunction() {
-        System.out.println("This is for lab 3 in CS 41600 \n:)");
+        return x + y;
     }
 
+    private static void alecsFunction() {
+        System.out.println("This is for lab 3 in CS 41600");
+    }
 
     static void davidsFunction() {
-        int rand_x,rand_y;
-        Random rand = new Random(); 
+        int rand_x, rand_y;
+        Random rand = new Random();
         rand x = rand.nextInt(rand_x);
         rand y = rand.nextInt(rand_y);
 
         System.out.println("Your lucky number is: " + (x + y));
     }
-    
+
 }
