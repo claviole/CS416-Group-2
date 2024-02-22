cdd90e9f325d40231a3ec1a9dcdec820bb604f79
drewjay14 <ajohnse@pnw.edu>
2024-02-22 15:52:59 -0600

    Drew Johnsen, First Commit

--- main.java
+++ main.java
@@ -1,7 +1,11 @@
 
-public class Main {
+public class main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        //Drew Johnsen
+        int x = 1, y = 2;
+        int result =  x * y;
+        System.out.println(result);
     }
 }
 
