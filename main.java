81d91f5129186d3296490699baee390fecb54ba9
NJL56 <nlachcik0506@gmail.com>
2024-02-22 16:11:43 -0600

    nick

--- main.java
+++ main.java
@@ -1,13 +1,19 @@
 
-public class Main {
+public class main {
 
 
 
 
 
     //Christian Laviolette
-    public static void christian(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(nick(4));
+    }
+
+    //Nick Lachcik
+    public static boolean nick(int n){
+        return n%2==0;
     }
 }
 
